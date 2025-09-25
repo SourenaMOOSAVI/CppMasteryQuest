21a909ee4845af8107b4f33aa0caa29f7edb88e2
Sourena MOOSAVI <stloverflow@gmail.com>
2025-09-25 23:02:43 +0200

    minor modification

---
+++ M1/W1/templatedStackVectBased.h
@@ -0,0 +1,38 @@
+#ifndef H_STACKVECTBASED
+#define H_STACKVECTBASED
+
+#include <stdexcept>
+#include <vector>
+
+template <typename T> class StackVectorBased {
+private:
+  std::vector<T> elements;
+
+public:
+  // Push an element onto the stack
+  void push(const T &value) { elements.push_back(value); }
+
+  // Pop (remove) an element from the stack
+  void pop() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    elements.pop_back();
+  }
+
+  // get the top element of the stack
+  T &top() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return elements.back();
+  }
+
+  // Check if the stack is empty
+  bool empty() const { return elements.empty(); }
+
+  // Get the size of the stack
+  size_t size() const { return elements.size(); }
+};
+
+#endif
